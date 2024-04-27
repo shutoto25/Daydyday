a5fd254aead870e43ca26d3e8966694bc0740bf7
shuto mohri <1go1e.ices2@gmail.com>
2024-04-27 15:44:04 +0900

    chore: libs_dokkaセットアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,8 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.android.library) apply false
+    // TODO tureにするとタスクが通るがfalseとの違いがわからない
+    //  https://blog.mrhaki.com/2016/09/gradle-goodness-add-but-do-not-apply.html
+    alias(libs.plugins.org.jetbrains.dokka) apply true
 }
 true // Needed to make the Suppress annotation work for the plugins block

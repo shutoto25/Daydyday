5eb8e1b3db87861a84388e99338c4d338b50d55e
shuto mohri <1go1e.ices2@gmail.com>
2024-12-10 22:13:48 +0900

    feature: FABタップ時に今日に移動する

--- feature/src/main/java/com/gmail/shu10/dev/app/feature/home/HomeScreen.kt
+++ feature/src/main/java/com/gmail/shu10/dev/app/feature/home/HomeScreen.kt
@@ -1,14 +1,15 @@
 package com.gmail.shu10.dev.app.feature.home
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -19,13 +20,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.gmail.shu10.dev.app.feature.theme.DaydydayTheme
+import kotlinx.coroutines.launch
 
 /**
  * ホーム画面(日付リスト)
@@ -34,19 +39,38 @@ import com.gmail.shu10.dev.app.feature.theme.DaydydayTheme
 @Composable
 fun InfiniteDateList(navController: NavController) {
     val viewModel: HomeViewModel = viewModel()
+    val coroutineScope = rememberCoroutineScope()
     val dateList by viewModel.dateList.collectAsState()
     // リスト初期位置は今日
     val listState = rememberLazyListState(initialFirstVisibleItemIndex = 365)
+    // FAB表示フラグ（今日に近い場合はFABを表示しない）
+    val isFabVisible by remember {
+        derivedStateOf { listState.firstVisibleItemIndex != 365 }
+    }
+    // FABアイコン（今日を基準に過去は下向きアイコン、未来は上向きアイコンを設定）
+    val fabIcon by remember {
+        derivedStateOf {
+            if (listState.firstVisibleItemIndex < 365)
+                Icons.Default.KeyboardArrowDown
+            else Icons.Default.KeyboardArrowUp
+        }
+    }
 
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                Log.d("TAG", "FloatingActionButton clicked!")
-            }) {
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = "scroll to today's position"
-                )
+            if (isFabVisible) {
+                FloatingActionButton(
+                    shape = RoundedCornerShape(50),
+                    onClick = {
+                        coroutineScope.launch {
+                            listState.animateScrollToItem(index = 365)
+                        }
+                    }) {
+                    Icon(
+                        imageVector = fabIcon,
+                        contentDescription = "scroll to today's position"
+                    )
+                }
             }
         },
         floatingActionButtonPosition = FabPosition.Center

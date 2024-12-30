4a46c52fa0211d5575e2593b33d4bf303c306a92
shuto mohri <1go1e.ices2@gmail.com>
2024-12-30 11:34:21 +0900

    feature: coreモジュールのリソースエイリアスを追加整理

---
+++ core/src/main/java/com/gmail/shu10/dev/app/core/CoreResourceAlias.kt
@@ -0,0 +1,11 @@
+package com.gmail.shu10.dev.app.core
+
+/**
+ * Coreモジュールリソースのエイリアス
+ */
+typealias CoreDrawable = R.drawable
+typealias CoreColor = R.color
+typealias CoreDimen = R.dimen
+typealias CoreId = R.id
+typealias CoreInteger = R.integer
+typealias CoreString = R.string
--- core/src/main/java/com/gmail/shu10/dev/app/core/CoreString.kt
+++
@@ -1,6 +0,0 @@
-package com.gmail.shu10.dev.app.core
-
-/**
- * Core string resources
- */
-typealias CoreString = R.string

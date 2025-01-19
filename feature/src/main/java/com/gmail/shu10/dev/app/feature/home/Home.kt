de20e3889e3dedea2aeb63edf9167ebe6a8270c0
shuto mohri <1go1e.ices2@gmail.com>
2025-01-19 10:47:16 +0900

    feature: import削除

--- feature/src/main/java/com/gmail/shu10/dev/app/feature/home/Home.kt
+++ feature/src/main/java/com/gmail/shu10/dev/app/feature/home/Home.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp

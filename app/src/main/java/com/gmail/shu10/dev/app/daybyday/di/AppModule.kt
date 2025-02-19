ff9e2569a67c109477fcbaa1bec9c70155d522e7
shuto mohri <1go1e.ices2@gmail.com>
2025-02-19 22:05:14 +0900

    feature: SharedPreference用のリポジトリ作成

--- app/src/main/java/com/gmail/shu10/dev/app/daybyday/di/AppModule.kt
+++ app/src/main/java/com/gmail/shu10/dev/app/daybyday/di/AppModule.kt
@@ -1,10 +1,12 @@
 package com.gmail.shu10.dev.app.daybyday.di
 
+import android.content.Context
 import com.gmail.shu10.dev.app.data.SharedPreferenceRepository
 import com.gmail.shu10.dev.app.domain.ISharedPreferenceRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @InstallIn(SingletonComponent::class)
@@ -12,5 +14,6 @@ import dagger.hilt.components.SingletonComponent
 class AppModule {
 
     @Provides
-    fun provideSharedPreferenceRepository(): ISharedPreferenceRepository = SharedPreferenceRepository()
+    fun provideSharedPreferenceRepository(@ApplicationContext context: Context): ISharedPreferenceRepository =
+        SharedPreferenceRepository(context)
 }
